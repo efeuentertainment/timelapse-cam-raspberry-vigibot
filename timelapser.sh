4057f959c9b41beca7eb0ef234723c2bf6aa2d77
root <github@whatimean.ch>
2022-11-22 10:34:08 +0100

    moar

--- timelapser.sh
+++ timelapser.sh
@@ -22,8 +22,8 @@ do
     echo "found hints of HDR pictures. set hw config -> EXPOSUREBRACKETING to 0 and delete /tmp/*.jpg (or reboot). discarding snapshot..."
     rm /tmp/out.jpg
   else
-    mv /tmp/out.jpg /home/pi/timelapse/"$(date +%Y%m%d_%H%M%S_%3N)".jpg
-    find /home/pi/timelapse/ -mtime +1 -delete
+    mv /tmp/out.jpg "$dir $(date +%Y%m%d_%H%M%S_%3N)".jpg
+    find "$dir" -mtime +1 -delete
 
     if [ $(ls -1 $dir | wc -l) -lt 61 ]; then
       echo "new snapshot found. timelapse_long might need as many as 60 frames to work properly. current count: $(ls -1 $dir | wc -l)"
@@ -35,14 +35,14 @@ do
       if [[ ! -f /tmp/timelapse_short.mp4 || $(find /tmp/timelapse_short.mp4 -mmin +5) ]]; then
 	#date
 	echo "creating timelapse_short.mp4 ..."
-	ffmpeg -sseof -2 -r 10 -pattern_type glob -i "/home/pi/timelapse/*.jpg" -s 640x480 -vcodec libx264 -filter:v fps=fps=30 /tmp/timelapse_short.mp4 -y >/dev/null 2>&1
+	ffmpeg -sseof -2 -r 10 -pattern_type glob -i "$dir*.jpg" -s 640x480 -vcodec libx264 -filter:v fps=fps=30 /tmp/timelapse_short.mp4 -y >/dev/null 2>&1
 	echo "timelapse_short.mp4 done!"
       fi
 
       if [[ ! -f /tmp/timelapse_long.mp4 || $(find /tmp/timelapse_long.mp4 -mmin +30) ]]; then
 	date
 	echo "creating timelapse_long.mp4 ..."
-	ffmpeg -sseof -52 -r 30 -pattern_type glob -i "/home/pi/timelapse/*.jpg" -filter:v "setpts=0.5*PTS" -s 640x480 -vcodec libx264 /tmp/timelapse_long.mp4 -y >/dev/null 2>&1
+	ffmpeg -sseof -52 -r 30 -pattern_type glob -i "$dir*.jpg" -filter:v "setpts=0.5*PTS" -s 640x480 -vcodec libx264 /tmp/timelapse_long.mp4 -y >/dev/null 2>&1
 	echo "timelapse_long.mp4 done!"
       fi
     fi
